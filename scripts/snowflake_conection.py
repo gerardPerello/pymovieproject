79afa135e5e572c1ade630749a0e42d5594e5268
Gerard Perello <gperelma20@alumnes.ub.edu>
2023-10-04 12:06:35 +0200

    changed connction

--- scripts/snowflake_conection.py
+++ scripts/snowflake_conection.py
@@ -20,18 +20,21 @@ def do_operation(operation_name, table_name, data_type, data):
         push_data(conn, table_name, data_type, data)
 
     #### ADD MORE
+    
+    # Close the connection 
+    cur.close() 
+    conn.close()
 
 def push_data(conn, table_name, data_type, data):
     # Create or use an existing table to hold the JSON 
 
-    cur = conn.cursor() 
-    cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (DATA VARIANT);") 
    
     # Insert the JSON data into the table 
     
     json_str = response.replace("'", "''") #response.text.replace("'", "''") # Escape single quotes 
     cur.execute(f"INSERT INTO {table_name} SELECT PARSE_JSON(COLUMN1) FROM VALUES ('{data}');") 
     
-    # Close the connection 
-    cur.close() 
-    conn.close() 
+
+
+def create_table(conn, table_name, data_query = '(DATA VARIANT)'):
+    cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} {data_query};") 
