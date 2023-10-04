ebe3939106857e33c00db2e06a747a913c7f17a5
Gerard Perello <gperelma20@alumnes.ub.edu>
2023-10-04 12:16:50 +0200

    Changing things api conn

--- scripts/api_conection.py
+++ scripts/api_conection.py
@@ -4,17 +4,29 @@ import sys
 sys.path.append('../scripts/credentials')
 
 # Define the API URL
-url = "http://apilayer.net/api/live"
+url = "http://api.currencylayer.com/historical"
 
-params = dict()
+def get_currencies_names(n=-1):
+    # Get 
+    file_path = '../data/currencies.json'
+    df = pd.read_json(file_path)
 
-def define_params(currencies = "EUR,GBP,CAD,PLN"):
+    currencies = ""
+    for a in df['Code'].values:
+        currencies += a + ","
+    
+    return currencies
+
+def define_params(date, currencies = "EUR,GBP,CAD,PLN"):
     params = {
         "access_key": os.getenv("API_KEY"),
         "currencies": currencies,
         "source": "USD",
-        "format": "1"
+        "format": "1",
+        'date' : date
     }
+
+    return params
 
 def get(url, params, print_ = False):
     # Make the GET request
