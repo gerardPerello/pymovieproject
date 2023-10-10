7c18c6a0eda43c915900201a3e48a7d925c49d5f
Edoardo Draetta <edraetta@gmail.com>
2023-10-10 15:46:22 +0200

    cleanup clientside

--- client/credentials/credentials.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["SNOWSQL_PWD"] = "iamSnowflake7"
-os.environ["SNOWSQL_USR"] = "edraetta"
-os.environ["SNOWSQL_ACC"] = "xc01903.eu-central-1"
-os.environ["SNOWSQL_WH"] = "compute_wh"
-os.environ["SNOWSQL_DB"] = "stocks_db"
-os.environ["SNOWSQL_SCH"] = "public"
-os.environ["API_KEY"] = "b3f26ec6e245adfd20b78d596e16a343"
-
--- client/credentials/credentials_format.py
+++
@@ -1,8 +0,0 @@
-import os
-os.environ["SNOWSQL_PWD"] = ""
-os.environ["SNOWSQL_USR"] = ""
-os.environ["SNOWSQL_ACC"] = ""
-os.environ["SNOWSQL_WH"] = ""
-os.environ["SNOWSQL_DB"] = ""
-os.environ["SNOWSQL_SCH"] = ""
-os.environ["API_KEY"] = ""
